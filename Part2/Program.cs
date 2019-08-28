f90ef5e3f6f6b402ee6181316b0626aa548018c4
Conor Doherty <conordoherty@conors-mbp.wireless.rit.edu>
2019-08-28 10:04:49 -0400

    Initial commit

---
+++ Part2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MAD students - I'm Conor!");
+             Console.WriteLine("I'm from Williamsburg, VA");
+
+        }
+    }
+}
