d9771aec9f2a9d57522fa9e79e04920f4eda8dcd
Conor Doherty <conordoherty@conors-mbp.wireless.rit.edu>
2019-08-30 09:40:04 -0400

    Part 3

---
+++ part 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("");
+            Console.WriteLine("Hello MAD students - I'm Conor!");
+            Console.WriteLine("");
+            Console.WriteLine("I'm from Williamsburg, VA");
+            Console.WriteLine("");
+        }
+    }
+}
